0584d12705c8d296fdf69ad84349b352fbd186f2
audyrtl <100162512+audyrtl@users.noreply.github.com>
2022-03-01 10:46:11 +0700

    Create ComputeMethods502621103.java

---
+++ ComputeMethods502621103.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class ComputeMethods5026211103 {
+
+	//Temperature
+	public double fToC(double degreesF) {
+		return(5.0/9*(degreesF-32));
+	}
+
+	//Hypotenuse
+	public double hypotenuse(int a, int b) {
+		return(Math.sqrt(a*a+b*b));
+	}
+
+	//Dice
+	public int roll() {
+		Random rndm = new Random();
+			int Roll1 = rndm.nextInt(7)+1;
+			int Roll2 = rndm.nextInt(7)+1;
+		return(Roll1+Roll2);
+	}
+
+}
